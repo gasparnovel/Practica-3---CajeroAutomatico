aa5accdf26bea3863f7da6db90d53f4bafce64d5
gasparnovel <gnovel@cifpfbmoll.eu>
2021-05-29 13:55:21 +0200

    feat: :sparkles: Creación de todos los métodos de la aplicación
    Creamos todos lo métodos que la aplicacion necesita para funcionar

--- src/CajeroAutomatico.java
+++ src/CajeroAutomatico.java
@@ -1,12 +1,50 @@
-
+import java.util.ArrayList;
 
 public class CajeroAutomatico {
 
+    private int idUltCaj = 0;
+    private int idCaj;
+    private int[][] billetes;
+    private ArrayList<Object> listaTarjeta;
+
+    public void CajeroAutomatico() {
+        this.setIdCaj(idUltCaj++);
+        listaTarjeta = new ArrayList<Object>();
+    }
+
     public Object getListaTarjeta() {
-        return null;
+        return this.listaTarjeta;
     }
 
     public void setBilletes(int[][] carga_billetes) {
+        this.billetes = carga_billetes;
     }
 
+    public int getIdUltCaj() {
+        return this.idUltCaj;
+    }
+
+    public void setIdUltCaj(int idUltCaj) {
+        this.idUltCaj = idUltCaj;
+    }
+
+    public int getIdCaj() {
+        return this.idCaj;
+    }
+
+    public void setIdCaj(int idCaj) {
+        this.idCaj = idCaj;
+    }
+
+    public int[][] getBilletes() {
+        return this.billetes;
+    }
+
+    public void setListaTarjeta(ArrayList<Object> listaTarjeta) {
+        this.listaTarjeta = listaTarjeta;
+    }
+    
 }
+
+
+
